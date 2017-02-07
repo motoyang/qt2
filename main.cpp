dd7f544b1a9a73c43026fb0f8590e1b3de6f4ce9
moto2 yang <moto2yang@UltrabookPro-c16b.local>
2017-02-07 14:58:39 +0800

    first commit.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <QApplication>
+#include <QTranslator>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator *t = new QTranslator;
+//    t->load("/Users/moto2yang/cpp/qt/HelloChina/lang_zh.qm");
+    t->load("/Users/moto2yang/cpp/qt/HelloChina/lang_en.qm");
+    a.installTranslator(t);
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
